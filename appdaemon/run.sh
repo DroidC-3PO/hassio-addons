5963f2fbdb5a828d469562b5794ae36ef2d6c49f
DroidC-3PO <Lu6aC1gaF9vAt4iD5veb8j@von.one>
2017-06-13 08:42:33 +0200

    Move to local test

--- appdaemon/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-
-# start server
-exec appdaemon -c /config/appdaemon.cfg < /dev/null
--- snapcast/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-
-# start server
-exec snapserver -s "spotify:///usr/local/bin/librespot?name=Spotify"  < /dev/null
